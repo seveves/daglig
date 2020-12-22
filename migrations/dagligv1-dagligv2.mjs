da678fad6a2b5f8fb19ee70fe052ecf3b4f95747
Severin <sevefriede@gmail.com>
2020-12-22 22:53:21 +0100

    made migration script a bit more fail safe

--- migrations/dagligv1-dagligv2.mjs
+++ migrations/dagligv1-dagligv2.mjs
@@ -1,9 +1,15 @@
 import daglig from '../models/daglig.js';
 
 const up = async () => {
-  const dagligs = await daglig.DagligV1.find({});
-  if (dagligs) {
-    await daglig.DagligV2.insertMany(dagligs);
+  try {
+    if (daglig && daglig.DagligV1 && daglig.DagligV2) {
+      const dagligs = await daglig.DagligV1.find({});
+      if (dagligs) {
+        await daglig.DagligV2.insertMany(dagligs);
+      }
+    }
+  } catch (err) {
+    console.log('error while migrating dagligv1 to dagligv2', err);
   }
 };
 
--- migrations/migrate.mjs
+++ migrations/migrate.mjs
@@ -12,21 +12,27 @@ mongoose
   .then((db) => {
     if (db.connections[0].readyState) {
       const files = fs.readdirSync('migrations');
-      files
-        .filter((file) => file.indexOf('migrate') === -1)
-        .forEach((file) => {
-          import(`./${file}`).then((migration) => {
-            Promise.all([migration.up(), migration.down()])
-              .then(() => {
-                console.log(`migrated ${file}`);
-                process.exit(0);
-              })
-              .catch((err) => {
-                console.error(`error while migrated ${file}`, err);
-                process.exit(1);
-              });
-          });
+      const migrations = files.filter((file) => file.indexOf('migrate') === -1);
+      if (migrations.length === 0) {
+        console.log('nothing to migrate');
+        return process.exit(0);
+      }
+      migrations.forEach((file) => {
+        import(`./${file}`).then((migration) => {
+          Promise.all([migration.up(), migration.down()])
+            .then(() => {
+              console.log(`migrated ${file}`);
+              process.exit(0);
+            })
+            .catch((err) => {
+              console.error(`error while migrated ${file}`, err);
+              process.exit(1);
+            });
         });
+      });
+    } else {
+      console.error('mongodb connection error', err);
+      process.exit(1);
     }
   })
   .catch((err) => {
